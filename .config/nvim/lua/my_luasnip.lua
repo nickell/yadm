49e48e0ed1e1eb2e7b9074cb0a3ec8ca426207a9
Chad Nickell <nickell.chad@gmail.com>
2022-11-14 21:59:46 -0500

    luasnip stuff

---
+++ .config/nvim/lua/my_luasnip.lua
@@ -0,0 +1,11 @@
+local ls = require 'luasnip'
+local keymaps = require 'keymaps'
+
+keymaps.luasnip(ls)
+
+ls.setup { history = false, updateevents = 'TextChanged,TextChangedI' }
+
+ls.filetype_extend('typescript', { 'javascript' })
+ls.filetype_extend('typescriptreact', { 'typescript', 'javascript' })
+
+require('luasnip.loaders.from_lua').load { paths = '~/.config/nvim/lua/snippets' }
